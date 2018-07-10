e732aabbd9e97f7ce0e8a03c78dcdf45efc4efac
Paul Suarez <paulsblogging.com@gmail.com>
2018-07-09 18:29:37 -0700

    completed

--- src/D01_ArrayReverseTest.java
+++ src/D01_ArrayReverseTest.java
@@ -3,6 +3,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class D01_ArrayReverseTest {
+
     @Test
     public void testEmpty() throws Exception {
         int[] numbers = {};
@@ -18,6 +19,15 @@ class D01_ArrayReverseTest {
         D01_ArrayReverse.doubler(numbers);
 
         int[] expected = {2, 4, 6, 8, 10};
+        assertArrayEquals(expected, numbers);
+    }
+
+    @Test
+    public void testReverse() throws Exception {
+        int[] numbers = {1, 2, 3, 4, 5};
+        D01_ArrayReverse.reverse(numbers);
+
+        int[] expected = {5, 4, 3, 2, 1};
         assertArrayEquals(expected, numbers);
     }
 }
