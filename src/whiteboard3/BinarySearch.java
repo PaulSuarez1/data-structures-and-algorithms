85166598fd961029ee14a32e78bed791f83ff680
Paul Suarez <paulsblogging.com@gmail.com>
2018-07-12 12:08:20 -0700

    BinarySearch

--- src/whiteboard3/BinarySearch.java
+++ src/whiteboard3/BinarySearch.java
@@ -1,4 +1,22 @@
 package whiteboard3;
 
+
 public class BinarySearch {
+
+
+    public static int BinarySearch(int[] arr, int value, int low, int high) {
+
+        while (low <= high) {
+            int middle = low + (high - low) / 2;
+            if (value < arr[middle]) {
+                high = middle - 1;
+
+            } else if (value > arr[middle]) {
+                low = middle +1;
+            } else {
+                return middle;
+            }
+        }
+        return -1;
+    }
 }
--- src/whiteboard3/BinarySearchTest.java
+++ src/whiteboard3/BinarySearchTest.java
@@ -1,4 +1,36 @@
 package whiteboard3;
 
-public class BinarySearchTest {
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BinarySearchIterativeTest {
+
+    @Test
+    void numberSearchOne() {
+        int[] arr = {4,8,15,16,23,42};
+        int low = 0;
+        int high = arr.length - 1;
+        int value = 15;
+        int expected = 2;
+        int result = whiteboard3.BinarySearch.BinarySearch(arr, value, low, high);
+
+        assertEquals(expected, result);
+        System.out.println("The number was found at index: " + result);
+    }
+
+    @Test
+    void numberSearchTwo() {
+        int[] arr = {11,22,33,44,55,66,77};
+        int low = 0;
+        int high = arr.length - 1;
+        int value = 90;
+        int expected = -1;
+        int result = whiteboard3.BinarySearch.BinarySearch(arr, value, low, high);
+
+        assertEquals(expected, result);
+        System.out.println("The number was found at index: " + result);
+    }
+
+
 }
