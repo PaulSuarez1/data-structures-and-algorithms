950b2595e220c46fc0e79f0b65cfa249a47f44fd
Paul Suarez <paulsblogging.com@gmail.com>
2018-07-17 11:30:41 -0700

    got the before and after working

--- src/whiteboard6/Main.java
+++ src/whiteboard6/Main.java
@@ -7,10 +7,15 @@ public class Main {
         ll.prepend(5);
         ll.prepend(1);
         ll.prepend(22);
-        ll.prepend(33);
-        ll.prepend(4);
-        ll.prepend(56);
-        ll.prepend(30);
+
+        ll.append(99);
+        ll.append(100);
+        ll.append(101);
+
+        ll.insertBefore(99, 10000);
+
+        ll.insertAfter(1, 8888);
+
 
         if (ll.isEmpty()) {
             System.out.println("empty foo");
@@ -19,6 +24,11 @@ public class Main {
         System.out.println("ll: " + ll.size());
 
         ll.printList(ll.size());
+
+        System.out.println("Input index in get. Output value: " + ll.get(2));
+
+
+
 
     }
 
