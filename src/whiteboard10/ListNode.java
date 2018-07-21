122383b0e3ce58c26185b54856c21f40112b0116
Paul Suarez <paulsblogging.com@gmail.com>
2018-07-21 13:32:26 -0700

    going to new branch

---
+++ src/whiteboard10/ListNode.java
@@ -0,0 +1,13 @@
+package whiteboard10;
+
+public class ListNode {
+    public int data;
+    public ListNode next;
+
+    public ListNode(int data) {
+        this.data = data;
+        this.next = null;
+    }
+
+
+}
