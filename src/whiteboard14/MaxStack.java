7c69b71556c02c526dbfa8c29aad9d09920763f9
Paul Suarez <paulsblogging.com@gmail.com>
2018-07-27 10:33:15 -0700

    hmmm not really sure how to do this...

---
+++ src/whiteboard14/MaxStack.java
@@ -0,0 +1,20 @@
+package whiteboard14;
+
+import java.util.Stack;
+
+public class MaxStack extends Stack {
+
+    Stack<Integer> stack1 = new Stack();
+    Stack<Integer> stack2 = new Stack();
+
+    public static int max() {
+        int max = 0;
+
+        
+
+        return max;
+    }
+
+
+
+}
