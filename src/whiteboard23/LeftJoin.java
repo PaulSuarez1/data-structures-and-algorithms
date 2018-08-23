87df57120e2b38d96e425aef0afdc33e9c7ac016
Paul Suarez <paulsblogging.com@gmail.com>
2018-08-23 09:57:45 -0700

    I think this is headed in the right direction idk why the whiteboard linked to SQL though

---
+++ src/whiteboard23/LeftJoin.java
@@ -0,0 +1,22 @@
+package whiteboard23;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class LeftJoin {
+
+    public Map mergeMaps (Map<String, String> firstMap, Map<String, String>  secondMap) {
+        firstMap = new HashMap<>();
+        secondMap = new HashMap<>();
+
+        Map<String, ArrayList> joinedMap = new HashMap<>();
+        List<String> bothValues = new ArrayList<>();
+
+
+
+    }
+
+
+}
