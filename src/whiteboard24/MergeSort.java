8308c08d369dbf5dc4d5b8afa83d4d7f22a1131f
Paul Suarez <paulsblogging.com@gmail.com>
2018-08-28 13:49:53 -0700

    starting

---
+++ src/whiteboard24/MergeSort.java
@@ -0,0 +1,10 @@
+package whiteboard24;
+
+public class MergeSort {
+    public void mergeSort(int[] input, int left, int middle, int right) {
+
+        while (left < right) {
+            
+        }
+    }
+}
