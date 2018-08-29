450ef92ff249ff84e7c75c680b09c7aba25cad9a
Paul Suarez <paulsblogging.com@gmail.com>
2018-08-29 10:58:55 -0700

    i think this will work

--- src/whiteboard24/MergeSort.java
+++ src/whiteboard24/MergeSort.java
@@ -1,10 +1,33 @@
 package whiteboard24;
 
 public class MergeSort {
-    public void mergeSort(int[] input, int left, int middle, int right) {
 
-        while (left < right) {
+    private int[] array;
+    private int[] tempArray;
+    private int length;
 
+    public static void mergeSort() {
+
+        // creating testing array for now
+        int[] inputArr = {35,24,31,29,17,48,6,38,6,4};
+        MergeSort newMerge = new MergeSort();
+        newMerge.sort(inputArr);
+        for(int i : inputArr) {
+            System.out.println(i);
+            System.out.println(" ");
         }
+
     }
+
+    public void sort(int inputArr[]) {
+        this.array = inputArr;
+        this.length = inputArr.length;
+        this.tempArray = new int[length];
+        sorting(0, length - 1);
+    }
+
+    private void sorting (int low, int middle, int high) {
+        
+    }
+
 }
