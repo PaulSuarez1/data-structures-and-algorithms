45b7414a7f210dac1dbef9cc874e44068d1a1fc9
Paul Suarez <paulsblogging.com@gmail.com>
2018-08-29 12:14:25 -0700

    had to start over

--- src/whiteboard24/MergeSortTest.java
+++ src/whiteboard24/MergeSortTest.java
@@ -1,15 +1,5 @@
-package whiteboard24;
-
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class MergeSortTest {
 
-    @Test
-    void mergeSort() {
-
-        assertEquals(0,0);
-
-    }
 }
